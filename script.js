58e495d5fbd5be162cbe84eb4f96228f2f9c2202
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-03-31 11:20:57 -0300

    terminando a funcao de somar

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 const http = require('http')
+const url = require('url')
 const {soma} = require('./teste')
+
 
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
     const urlCapturada = url.parse(req.url,true)
     const {query} = urlCapturada
-    console.log(query)
-   let valores = req.url.replace('/', '').replace('?','').split('&') 
-   let parametros = {}
-   for(item in valores){
-    let chaveValor = valores[item].split('=')
-   parametros[chaveValor[0]] = Number(chaveValor[1])
+  let resultado = soma(Number(query.a), Number(query.b))
+  res.end(`O valor final é ${resultado}`)
 
    }
-   
-    let resultado = soma(parametros.a, parametros.b)
-    res.end(`O valor final e ${resultado}`)
-
-})
+   )
   
 
 server.listen(PORT, () => {
