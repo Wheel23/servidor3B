ecb002ba7eda7e0cfd0650ea91a2a33601c6d788
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-03-31 11:00:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const {soma} = require('./teste')
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+   let valores = req.url.replace('/', '').replace('?','').split('&') 
+   let parametros = {}
+   for(item in valores){
+    let chaveValor = valores[item].split('=')
+   parametros[chaveValor[0]] = Number(chaveValor[1])
+
+   }
+   
+    let resultado = soma(parametros.a, parametros.b)
+    res.end(`O valor final e ${resultado}`)
+
+})
+  
+
+server.listen(PORT, () => {
+    console.log(` o servidor foi inicializado na porta ${PORT}`)
+})
