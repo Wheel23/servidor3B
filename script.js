eb2dfb81953eb1cd07be8563ee2d42551589c8d7
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-04-02 11:11:20 -0300

    desenvolvendo outro jeito de criar o server

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-const http = require('http')
-const url = require('url')
+const express = require('express');
+const server = express()
+
+
+server.get('/soma', (req,res) => {
+    resultado = soma(req.query.a, req.query.b)
+    res.send(`O resultado da soma é: ${resultado}`)
+})
+
+server.get('/subtracao', (req,res) => {
+    resultado = subtracao(req.query.a, req.query.b)
+    res.send(`O resultado da subtracao é: ${resultado}`)
+})
+
+server.get('/divisao', (req,res) => {
+    resultado = divisao(req.query.a, req.query.b)
+    res.send(`O resultado da divisao é: ${resultado}`)
+})
+
+server.get('/multiplicacao', (req,res) => {
+    resultado = multiplicacao(req.query.a, req.query.b)
+    res.send(`O resultado da multiplicacao é: ${resultado}`)
+})
+
+
+
 const {soma, subtracao, divisao, multiplicacao} = require('./teste')
 const path = require('path')
 
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-    const urlCapturada = url.parse(req.url,true)
-    const {query, pathname} = urlCapturada
-    let resultado = 0
-    if(pathname == '/soma'){
-        resultado = soma(Number(query.a), Number(query.b))
-    }else if(pathname == '/subtracao'){
-        resultado = subtracao(Number(query.a), Number(query.b))
-    }else if(pathname == '/divisao'){
-        resultado = divisao(Number(query.a), Number(query.b))
-   }else if(pathname == '/multiplicacao'){
-    resultado = multiplicacao(Number(query.a), Number(query.b))
-   }else{
-    res.statusCode = 404
-    res.end('Pagina nao encontrada')
-   }
-   res.end(`O valor final e ${resultado}`)
-}
-   )
    
 
 server.listen(PORT, () => {
